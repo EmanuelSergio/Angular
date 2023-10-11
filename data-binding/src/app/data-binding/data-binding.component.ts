47da20fde6dd3eebe47f1ca9a1f2a912f17196ba
Emanuel Sergio Girardi <104005334+EmanuelSergio@users.noreply.github.com>
2023-10-11 16:29:49 -0300

    comunicações entre DOM e Component

--- data-binding/src/app/data-binding/data-binding.component.ts
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -10,8 +10,27 @@ export class DataBindingComponent {
 url:string = 'http://loiane.com'
 urlImagen:string = 'http://lorempixel.com.br/600/400'
 cursoAngular:boolean = true
+valorAtual:string = '';
+valorSalvo:string = '';
+isMouseOver:boolean=false
 
+nome:any='abc'
 
+onMouseOverOut(){
+  this.isMouseOver = !this.isMouseOver
+}
+
+salvarValor(valor:string){
+  this.valorSalvo = valor
+}
+
+onKeyUp(evento:KeyboardEvent){
+  this.valorAtual = (<HTMLInputElement>evento.target).value
+}
+
+botaoClicado(){
+  alert('botao acessado')
+}
 
 getValor(){
   return 1
